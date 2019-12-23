70ea41f59891bda6875f3816a9a7732ba4272f6b
dev-DTECH <deep64222@gmail.com>
2019-12-23 22:57:32 +0530

    adding debug mode

--- engine/GAME.js
+++ engine/GAME.js
@@ -6,7 +6,9 @@ let GAME = {
 		this.canvas = getcanvas;
 		this.ctx = getcanvas.getContext("2d");
 		// this.last_scale=0;
-
+	},
+	debug: function(ob) {
+		ob.debugmode = true;
 	},
 	object: class {
 		constructor(str, a) {
@@ -19,61 +21,54 @@ let GAME = {
 			this.ax = 0;
 			this.ay = 0;
 
+			this.debugmode = false;
+
 			if (str == "square")
 				this.points = [
-					{x:a, y:a},
-					{x:-a, y:a},
-					{x:-a, y:-a},
-					{x:a,y: -a}
+					{ x: a, y: a },
+					{ x: -a, y: a },
+					{ x: -a, y: -a },
+					{ x: a, y: -a }
 				];
-			this.polygon =new SAT.Polygon({x:this.x,y:this.y},this.points)
+			this.polygon = new SAT.Polygon({ x: this.x, y: this.y }, this.points);
 		}
 		addpoint(x, y) {
 			let find_angle = function(x, y) {
-				if(x>0&&y>0)
-				return Math.atan2(y,x)* 180 / Math.PI;
-				if(x<0&&y>0)
-				return Math.atan2(y,x)* 180 / Math.PI;
-				if(x>0&&y<0)
-				return (Math.PI*2+Math.atan2(y,x))* 180 / Math.PI;
-				if(x<0&&y<0)
-				return (Math.PI*2+Math.atan2(y,x))* 180 / Math.PI;
-				if(x==0)
-				{
-					if(y>0)
-					return 90
-					else
-					return 270
+				if (x > 0 && y > 0) return (Math.atan2(y, x) * 180) / Math.PI;
+				if (x < 0 && y > 0) return (Math.atan2(y, x) * 180) / Math.PI;
+				if (x > 0 && y < 0)
+					return ((Math.PI * 2 + Math.atan2(y, x)) * 180) / Math.PI;
+				if (x < 0 && y < 0)
+					return ((Math.PI * 2 + Math.atan2(y, x)) * 180) / Math.PI;
+				if (x == 0) {
+					if (y > 0) return 90;
+					else return 270;
 				}
-				if(y==0)
-				{
-					if(x>0)
-					return 0
-					else
-					return 180
+				if (y == 0) {
+					if (x > 0) return 0;
+					else return 180;
 				}
 				// return  (y >= 0&&x>=0 ? Math.atan2(y,x) : Math.abs(Math.atan2(y,x)))* 180 / Math.PI;
-			}
-			
+			};
+
 			let angle, angle1, angle2;
 
-			angle = find_angle(x,y);
+			angle = find_angle(x, y);
 
-			for (let i = 0; i < this.points.length-1 ; i++) {
+			for (let i = 0; i < this.points.length - 1; i++) {
 				angle1 = find_angle(this.points[i].x, this.points[i].y);
 				angle2 = find_angle(this.points[i + 1].x, this.points[i + 1].y);
-				console.log(angle);
-				console.log(angle1);
-				console.log(angle2);
+				// console.log(angle);
+				// console.log(angle1);
+				// console.log(angle2);
 
 				if (angle >= angle1 && angle <= angle2) {
 					for (let j = this.points.length - 1; j > i; j--) {
 						this.points[j + 1] = this.points[j];
 						// this.polygon.points[j+1]=this.polygon.points[j]
 					}
-					this.points[i+1] = {x:x, y:y};
+					this.points[i + 1] = { x: x, y: y };
 					// this.polygon.points[i+1] = {x:x, y:y};
-
 
 					break;
 				}
@@ -88,14 +83,12 @@ let GAME = {
 					break;
 				}
 			}
-			this.polygon =new SAT.Polygon({x:this.x,y:this.y},this.points)
-
+			this.polygon = new SAT.Polygon({ x: this.x, y: this.y }, this.points);
 		}
 	},
-	Response: new SAT.Response()
-	,
-	collisionsBetween: function(ob1,ob2){
-		return SAT.testPolygonPolygon(ob1.polygon, ob2.polygon, GAME.Response)
+	Response: new SAT.Response(),
+	collisionsBetween: function(ob1, ob2) {
+		return SAT.testPolygonPolygon(ob1.polygon, ob2.polygon, GAME.Response);
 	},
 	wall: {},
 	camera: {
@@ -116,61 +109,83 @@ let GAME = {
 		this.camera.x += this.camera.vx * dt;
 		this.camera.y += this.camera.vy * dt;
 
-		ob.vx += ob.ax * dt;
-		ob.vy += ob.ay * dt;
-
-		ob.x += ob.vx * dt;
-		ob.y += ob.vy * dt;
-
-		// ob.polygon.translate(ob.vx * dt, ob.vy * dt)
-
-		ob.polygon.pos.x = ob.x;
-		ob.polygon.pos.y = ob.y;
-
-
-		// for(let i=0;i<ob.polygon.points.length;i++){
-		// 	ob.polygon.points[i].x =ob.x+ob.points[i].x;
-		// 	ob.polygon.points[i].y =ob.y+ob.points[i].y;
-
-		// }
-		
+		// this.scale = scale;
+		let scale_points = function(ob, scale) {
+			let ar = [];
+			for (i = 0; i < ob.points.length; i++) {
+				ar[i] = { x: ob.points[i].x * scale, y: ob.points[i].y * scale };
+			}
+			ob.polygon = new SAT.Polygon({ x: ob.x, y: ob.y }, ar);
+		};
 
 		let scalex = this.canvas.width / 1000;
 		let scale = this.canvas.height / 1000;
-		
-		// if(scale!=this.scale){
-			console.log("resizing")
-			this.scale=scale;
-			let scale_points = function(ob,scale) {
-				let ar=[];
-				for(i=0;i<ob.points.length;i++){
-					ar[i]={x:ob.points[i].x*scale,y:ob.points[i].y*scale}
-				}
-				ob.polygon= new SAT.Polygon({x:ob.x,y:ob.y},ar)
-			
-			}
-			scale_points(ob,scale);
-		// }	
 
+		// if(scale!=this.scale){
+		// console.log("resizing")
+		scale_points(ob, scale);
+
+		// }
 
 		let kx = -this.camera.x + ob.x + this.canvas.width / 2;
 		let ky = this.camera.y - ob.y + this.canvas.height / 2;
 
+		if (!ob.debugmode) {
+			ob.vx += ob.ax * dt;
+			ob.vy += ob.ay * dt;
+
+			ob.x += ob.vx * dt;
+			ob.y += ob.vy * dt;
+
+			// ob.polygon.translate(ob.vx * dt, ob.vy * dt)
+
+			ob.polygon.pos.x = ob.x;
+			ob.polygon.pos.y = ob.y;
+		}
+
+		// for(let i=0;i<ob.polygon.points.length;i++){
+		// 	ob.polygon.points[i].x =ob.x+ob.points[i].x;
+		// 	ob.polygon.points[i].y =ob.y+ob.points[i].y;
+		// }
+
 		this.ctx.fillStyle = "#ab7def";
 
 		this.ctx.beginPath();
-		this.ctx.moveTo(
-			ob.points[0].x * scale + kx,
-			-ob.points[0].y * scale + ky
-		);
+		this.ctx.moveTo(ob.points[0].x * scale + kx, -ob.points[0].y * scale + ky);
+		// 			if(ob.debugmode){
+		// 		this.ctx.beginPath();
+		// this.ctx.arc(ob.points[0].x * scale + kx, -ob.points[0].y * scale + ky, 10, 0, 2 * Math.PI);
+		// this.ctx.fill();
+		// this.ctx.beginPath();
+
+		// }
 
 		for (i = 1; i < ob.points.length; i++) {
 			this.ctx.lineTo(
 				ob.points[i].x * scale + kx,
 				-ob.points[i].y * scale + ky
 			);
+			// 	if(ob.debugmode){
+			// 		this.ctx.beginPath();
+			// this.ctx.arc(ob.points[0].x * scale + kx, -ob.points[0].y * scale + ky, 10, 0, 2 * Math.PI);
+			// this.ctx.fill();
+			// this.ctx.beginPath();	}
 		}
 		this.ctx.fill();
+		if (ob.debugmode) {
+			this.ctx.fillStyle = "#ff0000";
+			for (i = 0; i < ob.points.length; i++) {
+				this.ctx.beginPath();
+				this.ctx.arc(
+					ob.points[i].x * scale + kx,
+					-ob.points[i].y * scale + ky,
+					10,
+					0,
+					2 * Math.PI
+				);
+				this.ctx.fill();
+			}
+		}
 	},
 	controller: class {
 		constructor(left, right, up, down) {
@@ -213,12 +228,11 @@ let GAME = {
 			}
 		}
 	}
-}
-let scale_points = function(ob,scale) {
-	let a=[];
-	for(i=0;i<ob.points.length;i++){
-		ar[i]={x:ob.points[i].x*scale,y:ob.points[i].y*scale}
+};
+let scale_points = function(ob, scale) {
+	let a = [];
+	for (i = 0; i < ob.points.length; i++) {
+		ar[i] = { x: ob.points[i].x * scale, y: ob.points[i].y * scale };
 	}
-	ob.polygon.points=ar;
-
-}
+	ob.polygon.points = ar;
+};
