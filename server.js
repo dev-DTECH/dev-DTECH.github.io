84c3459e1b5cc2a45e3a744e70dca0d5bab5284c
dev-dtech <deep64222@gmail.com>
2020-02-10 18:02:37 +0530

    a

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let io =require('socket.io')(process.env.PORT || 3000)
+console.log("Server online")
 
 io.on('connection',socket =>{
     console.log("user connected");
--- socket-io/server.js
+++ socket-io/server.js
@@ -1,4 +1,5 @@
 let io =require('socket.io')(3000)
+console.log("Server online")
 
 io.on('connection',socket =>{
     console.log("user connected");
